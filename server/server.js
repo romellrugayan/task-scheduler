a93529ba099d8562a34cd0d65491cf308f6239ee
Romell Rugayan <romell.rugayan@gmail.com>
2018-10-07 12:27:44 +0800

    Prepared for deployment

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
 
-const users = require("./routes/users");
-const tasks = require("./routes/tasks");
+const users = require('./routes/users');
+const tasks = require('./routes/tasks');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
@@ -20,12 +21,22 @@ mongoose
     db,
     { useNewUrlParser: true }
   )
-  .then(() => console.log("MongoDB connected..."))
+  .then(() => console.log('MongoDB connected...'))
   .catch(err => console.log(err));
 
 // Use routes
-app.use("/api/users", users);
-app.use("/api/tasks", tasks);
+app.use('/api/users', users);
+app.use('/api/tasks', tasks);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
