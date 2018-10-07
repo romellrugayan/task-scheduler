38c9a8f1106145ccaeb1cbafb106ee15c3d7ab73
Romell Rugayan <romell.rugayan@gmail.com>
2018-10-07 11:46:07 +0800

    Update keys.js for deployment to Heroku

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,11 @@
-module.exports = {
-  mongoURI: 'mongodb://localhost:27017/taskscheduler',
-  secretOrKey: 'superSecretKey'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
+  };
+} else {
+  module.exports = {
+    mongoURI: 'mongodb://localhost:27017/taskscheduler',
+    secretOrKey: 'superSecretKey'
+  };
+}
